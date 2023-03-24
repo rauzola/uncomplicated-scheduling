212f2fcfc5dfa5cd87ef3807f287f5c3911d0330
Raul Sigoli <raul_sigoli@hotmail.com>
2023-03-24 11:57:21 -0300

    feat: Estrutura visual da home

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,10 +1,1 @@
-import react from 'react'
-import { Heading, Text } from '@ignite-ui/react'
-
-export default function Home() {
-  return (
-    <>
-      <Heading as="h1">Hello World</Heading>
-    </>
-  )
-}
+export { default } from './home'
