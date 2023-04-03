d74d4401f4ae4fec6fa9819fc64f48bcf38ccec1
Raul Sigoli <raul_sigoli@hotmail.com>
2023-04-03 11:50:45 -0300

    feat: Persistindo usuário no Prisma

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,24 +1,27 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
-import { ArrowRight } from 'phosphor-react'
-import { useForm } from 'react-hook-form'
-import { Container, Form, FormError, Header } from './styles'
-import { z } from 'zod'
 import { useRouter } from 'next/router'
+import { ArrowRight } from 'phosphor-react'
 import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { api } from '../../lib/axios'
+
+import { Container, Form, FormError, Header } from './styles'
 
 const registerFormSchema = z.object({
   username: z
     .string()
-    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras' })
-    .regex(/^([a-z\\\\-]+)$/i, {
-      message: 'O usuário pode ter apenas letras e hifens',
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário pode ter apenas letras e hifens.',
     })
     .transform((username) => username.toLowerCase()),
   name: z
     .string()
-    .min(3, { message: 'O nome precisa ter pelo menos 3 letras' }),
+    .min(3, { message: 'O nome precisa ter pelo menos 3 letras.' }),
 })
+
 type RegisterFormData = z.infer<typeof registerFormSchema>
 
 export default function Register() {
@@ -40,7 +43,14 @@ export default function Register() {
   }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return (
@@ -48,8 +58,8 @@ export default function Register() {
       <Header>
         <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
         <Text>
-          Precisamos de algumas informações para criar seu perfil! ah, você pode
-          editar essas informações depois
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
         </Text>
 
         <MultiStep size={4} currentStep={1} />
@@ -60,17 +70,19 @@ export default function Register() {
           <Text size="sm">Nome de usuário</Text>
           <TextInput
             prefix="ignite.com/"
-            placeholder="seu-usuario"
+            placeholder="seu-usuário"
             {...register('username')}
           />
+
           {errors.username && (
             <FormError size="sm">{errors.username.message}</FormError>
           )}
         </label>
 
         <label>
-          <Text size="sm">Nome Completo</Text>
-          <TextInput placeholder="seu nome" {...register('name')} />
+          <Text size="sm">Nome completo</Text>
+          <TextInput placeholder="Seu nome" {...register('name')} />
+
           {errors.name && (
             <FormError size="sm">{errors.name.message}</FormError>
           )}
